f8f3e0768b4730edb00de8ac957cb7e5fcf72ae6
Armaan Gupta <armaan@guppy.im>
2023-01-22 18:03:53 -0600

    feat: added getLinks and modified routes

--- src/links/links.controller.ts
+++ src/links/links.controller.ts
@@ -2,6 +2,7 @@ import {
   Body,
   Controller,
   Delete,
+  Get,
   HttpCode,
   HttpStatus,
   Param,
@@ -9,12 +10,12 @@ import {
   Post,
 } from '@nestjs/common';
 
-import { IResponse } from 'types/IResponse';
-import logger from 'utils/logger';
+import { IResponse } from '../../types/IResponse';
+import logger from '../../utils/logger';
 import { NewLinkDto, UpdateLinkDto } from './links.dto';
 import { LinksService } from './links.service';
 
-@Controller('links')
+@Controller('v1/links')
 export class LinksController {
   constructor(private linksService: LinksService) {}
 
@@ -37,8 +38,17 @@ export class LinksController {
     // authentication for this method will happen in some sort of middleware
     // therefore the "ksn" and user details will come from cookies/headers
     // but not from the POST requst body, only the target for the new link
-    logger.info(`POST /links/user initiated with body: ${dto}`);
+    logger.info({ message: `POST /links/user initiated`, body: dto });
     return this.linksService.createUserLink(dto);
+  }
+
+  @Get('')
+  @HttpCode(HttpStatus.OK)
+  async getLinks(): Promise<IResponse> {
+    // this controller gets all of the links that our database currently holds
+    // eventually this method will be modified to get all links held by a certain KSN
+    logger.info(`GET /links initiated`);
+    return this.linksService.getLinks();
   }
 
   @Post(':id')
@@ -48,7 +58,7 @@ export class LinksController {
     @Body() dto: UpdateLinkDto,
   ): Promise<IResponse> {
     // update details for a link, like target and extension with a given ID
-    logger.info(`POST /${id} initiated with body: ${dto}`);
+    logger.info({ message: `POST /${id} initiated`, body: dto });
     return this.linksService.updateLink(id, dto);
   }
 

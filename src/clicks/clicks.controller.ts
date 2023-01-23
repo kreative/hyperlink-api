7bda77490dd9ff5a969b8b662ae47adc47ec116f
Armaan Gupta <armaan@guppy.im>
2023-01-22 18:03:15 -0600

    feat: created click controller with transformer

--- src/clicks/clicks.controller.ts
+++ src/clicks/clicks.controller.ts
@@ -1,4 +1,31 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  ParseIntPipe,
+  Req,
+} from '@nestjs/common';
+import logger from '../../utils/logger';
+import { ClicksService } from './clicks.service';
+import { IClientIpinfoRequest } from '../../types/IClientIpinfoRequest';
+import { IResponse } from '../../types/IResponse';
 
-@Controller('clicks')
-export class ClicksController {}
+// since the clicks controller is used to transform a link from an extension to a redirect
+// this controller has to be empty from a global Controller class perspective
+@Controller('')
+export class ClicksController {
+  constructor(private clicksService: ClicksService) {}
+
+  @Get(':extension')
+  @HttpCode(HttpStatus.OK)
+  async transformAndCreateClick(
+    @Req() req: IClientIpinfoRequest,
+    @Param('extension') extension: string,
+  ): Promise<IResponse> {
+    // creates a new click object, updates the link clickCount, and sends back target
+    logger.info(`GET /${extension} in ClicksController initiated`);
+    return this.clicksService.transformAndCreateClick(req, extension);
+  }
+}

5a240c6ce8d35542f5491f3233424911abfa610c
Armaan Gupta <armaan@guppy.im>
2023-01-22 18:03:25 -0600

    feat: updated imports and exports for clicks

--- src/clicks/clicks.module.ts
+++ src/clicks/clicks.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { LinksModule } from 'src/links/links.module';
 import { ClicksController } from './clicks.controller';
 import { ClicksService } from './clicks.service';
 
 @Module({
+  imports: [LinksModule],
   controllers: [ClicksController],
-  providers: [ClicksService]
+  providers: [ClicksService],
+  exports: [ClicksService]
 })
 export class ClicksModule {}

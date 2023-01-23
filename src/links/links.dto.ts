aa7c7b753f83cdda2711c068ca5a13f3180db90b
Armaan Gupta <armaan@guppy.im>
2023-01-22 18:04:39 -0600

    fix: removed duplicate isString from imports

--- src/links/links.dto.ts
+++ src/links/links.dto.ts
@@ -1,4 +1,4 @@
-import { IsBoolean, IsNotEmpty, IsString, IsUrl } from "class-validator";
+import { IsBoolean, IsNotEmpty, IsString, IsUrl } from 'class-validator';
 
 export class NewLinkDto {
   @IsNotEmpty()

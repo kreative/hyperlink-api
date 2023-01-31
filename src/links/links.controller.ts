e9d655dd48d42e5e998d5f7f9796c2b984123ec2
Armaan Gupta <armaan@guppy.im>
2023-01-30 23:06:01 -0600

    feat: added request objects to controller

--- src/links/links.controller.ts
+++ src/links/links.controller.ts
@@ -8,7 +8,9 @@ import {
   Param,
   ParseIntPipe,
   Post,
+  Req,
 } from '@nestjs/common';
+import { IAuthenticatedRequest } from 'types/IAuthenticatedRequest';
 
 import { IResponse } from '../../types/IResponse';
 import logger from '../../utils/logger';
@@ -33,42 +35,44 @@ export class LinksController {
 
   @Post('user')
   @HttpCode(HttpStatus.OK)
-  async createUserLink(@Body() dto: NewLinkDto): Promise<IResponse> {
+  async createUserLink(@Req() req: IAuthenticatedRequest, @Body() dto: NewLinkDto): Promise<IResponse> {
     // create a new account link (requires authentication)
     // authentication for this method will happen in some sort of middleware
     // therefore the "ksn" and user details will come from cookies/headers
     // but not from the POST requst body, only the target for the new link
     logger.info({ message: `POST /links/user initiated`, body: dto });
-    return this.linksService.createUserLink(dto);
+    return this.linksService.createUserLink(req, dto);
   }
 
   @Get('')
   @HttpCode(HttpStatus.OK)
-  async getLinks(): Promise<IResponse> {
+  async getLinks(@Req() req: IAuthenticatedRequest): Promise<IResponse> {
     // this controller gets all of the links that our database currently holds
     // eventually this method will be modified to get all links held by a certain KSN
     logger.info(`GET /links initiated`);
-    return this.linksService.getLinks();
+    return this.linksService.getLinks(req);
   }
 
   @Post(':id')
   @HttpCode(HttpStatus.OK)
   async updateLink(
+    @Req() req: IAuthenticatedRequest,
     @Param('id', ParseIntPipe) id: number,
     @Body() dto: UpdateLinkDto,
   ): Promise<IResponse> {
     // update details for a link, like target and extension with a given ID
     logger.info({ message: `POST /${id} initiated`, body: dto });
-    return this.linksService.updateLink(id, dto);
+    return this.linksService.updateLink(req, id, dto);
   }
 
   @Delete(':id')
   @HttpCode(HttpStatus.OK)
   async deactivateLink(
+    @Req() req: IAuthenticatedRequest,
     @Param('id', ParseIntPipe) id: number,
   ): Promise<IResponse> {
     // deactivate link, basically deleting a link using the ID
     logger.info(`DELETE /${id} initiated`);
-    return this.linksService.deactivateLink(id);
+    return this.linksService.deactivateLink(req, id);
   }
 }
